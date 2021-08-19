9f4cd5a39de7dd0ea2c090c5e94cabc195c6f550
dileepfrog <dileepfrog@gmail.com>
2021-08-18 17:58:39 -0700

    chore: revert visibility changes

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -268,9 +268,9 @@ contract Exchange is IExchange, Owned {
   // Custodian
   ICustodian _custodian;
   // Deposit index
-  uint64 public _depositIndex;
+  uint64 _depositIndex;
   // Exits
-  mapping(address => WalletExit) public _walletExits;
+  mapping(address => WalletExit) _walletExits;
   // Liquidity pools
   address _liquidityMigrator;
   LiquidityPools.Storage _liquidityPools;
